764b6e99e50f4a3d08a4f1d525e97302160348c7
Barragan17 <tegartrahman@gmail.com>
2021-08-30 23:20:04 +0700

    Add Category Navigation

---
+++ meals_app/lib/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+import 'category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeals App'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES
+            .map((catData) =>
+                CategoryItem(catData.id, catData.title, catData.color))
+            .toList(),
+        // use sliver to create a scrollable grid screen
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          // use childAspectRation to set how the item should be sized regarding their height and width relation
+          childAspectRatio: 3 / 2,
+          // use cross and main axis spacing to create a spacing between each items
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ meals_app/lib/category_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  CategoryItem(this.id, this.title, this.color);
+
+  void selectCategory(BuildContext ctx) {
+    // used pushnamed to use named route
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // use InkWell to create some kind of transition while clicking the widget
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        // to increase build performance use const
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              // use withOpacity to make the color more transparent
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          // use borderRadius to make the item more circular
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
